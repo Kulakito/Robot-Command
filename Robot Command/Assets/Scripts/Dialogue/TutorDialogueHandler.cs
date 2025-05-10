89684e523f5cd3e5180920bb09ba4f0e702c7a46
VolderMiles <161145456+VolderMiles@users.noreply.github.com>
2025-05-10 22:31:02 +0300

    Dialogue

--- Robot Command/Assets/Scripts/Dialogue/TutorDialogueHandler.cs
+++ Robot Command/Assets/Scripts/Dialogue/TutorDialogueHandler.cs
@@ -1,28 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
-using UnityEngine;
 using TMPro;
 
 public class TutorDialogueHandler : MonoBehaviour
 {
-    [SerializeField] string[] messages;
+    [SerializeField] DialogueMessage[] messages;
+    [SerializeField] bool activateOnStart = true;
 
-    [SerializeField] 
+    [SerializeField] TMP_Text te;
+    [SerializeField] GameObject panel;
+    [SerializeField] Image charImage;
 
-    int isViewed = 0;
+    //bool isActiveDialogue = false;
 
     void Start()
     {
-        isViewed = PlayerPrefs.GetInt($"TutorOnScene{SceneManager.GetActiveScene().buildIndex}Viewed");
-        if (isViewed == 0)
+        if (PlayerPrefs.GetInt($"TutorOnScene{SceneManager.GetActiveScene().buildIndex}Viewed") == 0 && activateOnStart)
         {
             StartCoroutine(ActivateDialogue());
+            PlayerPrefs.SetInt($"TutorOnScene{SceneManager.GetActiveScene().buildIndex}Viewed", 1);
         }
     }
 
-    public IEnumerator ActivateDialogue()
+    public void PlayDialogue()
     {
+        StartCoroutine(ActivateDialogue());
+    }
+
+    IEnumerator ActivateDialogue()
+    {
+        panel.SetActive(true);
+        //isActiveDialogue = true;
+        for (int i = 0; i < messages.Length; i++)
+        {
+            te.text = "";
+            charImage.sprite = messages[i].charSprite;
+            StartCoroutine(TextTyper.TypeText(messages[i].text, te, .025f));
+            yield return new WaitUntil(() => !TextTyper.isTyping && Input.GetKeyDown(KeyCode.Space));
+            yield return null;
+        }
+        //isActiveDialogue = false;
+        panel.SetActive(false);
         yield return null;
     }
+}
+[System.Serializable]
+public class DialogueMessage
+{
+    public Sprite charSprite;
+    public string text;
 }
